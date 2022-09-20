3978c341e2d5c0640edf39c19934b95f9e953045
Florian <caron.florian7620@gmail.com>
2022-09-20 16:21:30 +0200

    add bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // Le nom de balise du composant (voir son appel dans index.html)
+  templateUrl: './app.component.html', // Correspond au template/vue qui sera affichée à l'utilisateur
+  styleUrls: ['./app.component.css'], // Le style du composant
 })
 export class AppComponent {
-  title = 'demo-angular-2';
+  title = 'Leçon sur les liaisons'; // variable qui est appelée dans le template comme ceci : {{title}}
+  interpolation = 'Interpolation variable';
+  valeurInput = 'Property Binding';
+  onClickButton() {
+    console.log('Un clic a été effectué sur le bouton !');
+  }
+  zoneSaisie: any;
 }
